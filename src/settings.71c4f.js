73ec35d366f3afcf92457e3b23990bce91d8b889
Jun Jun <43845644+junjun1993@users.noreply.github.com>
2025-12-17 00:07:08 +0100

    Update

--- src/settings.71c4f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Lobby.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"bf22c",main:"b66e8"}};

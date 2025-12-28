5cab261bb9184b4ae90b67c276be47f9775ce324
Jun Jun <43845644+junjun1993@users.noreply.github.com>
2025-12-28 23:24:36 +0100

    New Build

---
+++ src/settings.4a963.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Splash.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"9c9f4",main:"bdd3e"}};

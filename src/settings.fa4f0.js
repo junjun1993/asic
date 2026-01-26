58cc01190541358180a69efc6df63cedde585c92
Jun Jun <43845644+junjun1993@users.noreply.github.com>
2026-01-26 21:55:48 +0800

    New Deployment

---
+++ src/settings.fa4f0.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Splash.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"30d4f",main:"4ce7b"}};
